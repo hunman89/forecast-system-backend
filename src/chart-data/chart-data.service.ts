057ab41969b29e8024652adc0b36e3957ecd1871
hunman89 <hunman89@gmail.com>
2021-11-02 22:25:46 +0900

    add. 환경변수

--- src/chart-data/chart-data.service.ts
+++ src/chart-data/chart-data.service.ts
@@ -18,7 +18,7 @@ export class ChartDataService {
     // 인증 key
     const p_cert_key = this.configService.get<string>('KAMIS_API_KEY');
     // 요정자 id
-    const p_cert_id = 'hunman';
+    const p_cert_id = this.configService.get<string>('KAMIS_ID');
     // 리턴타입
     const p_returntype = 'json';
     // stert
@@ -26,19 +26,19 @@ export class ChartDataService {
     // end
     const p_endday = this.getPastDay(1);
     // 01:소매 02:도매
-    const p_productclscode = '02';
+    const p_productclscode = '01';
     // 부류코드
-    const p_itemcategorycode = '100';
+    const p_itemcategorycode = this.configService.get<string>('CLASS_CODE');
     // 품종 코드
-    const p_kindcode = '01';
+    const p_kindcode = this.configService.get<string>('KIND_CODE');
     // 등급 코드
-    const p_productrankcode = '04';
+    const p_productrankcode = this.configService.get<string>('GRADE_CODE');
     //시군구 default = 전체
     //const p_countycode = '1101';
     // kg단위 환산여부 (y = 1kg 단위표시)
     const p_convert_kg_yn = 'N';
     //품목코드
-    const p_itemcode = '111';
+    const p_itemcode = this.configService.get<string>('ITEM_CODE');
     const url = `http://www.kamis.or.kr/service/price/xml.do?action=periodProductList&p_startday=${p_startday}&p_endday=${p_endday}&p_productclscode=${p_productclscode}&p_itemcategorycode=${p_itemcategorycode}&p_itemcode=${p_itemcode}&p_kindcode=${p_kindcode}&p_productrankcode=${p_productrankcode}&p_convert_kg_yn=${p_convert_kg_yn}&p_cert_key=${p_cert_key}&p_cert_id=${p_cert_id}&p_returntype=${p_returntype}`;
 
     const {
@@ -91,7 +91,14 @@ export class ChartDataService {
   predictData(chartData: ChartData): ChartData {
     const length = chartData.labels.length;
     const lastDay = new Date(Date.parse(chartData.labels[length - 1]));
-    const lastPrice = chartData.kamisData[length - 1];
+
+    let i = 1;
+    let lastPrice: number = null;
+    while (!lastPrice) {
+      lastPrice = chartData.kamisData[length - i];
+      i++;
+    }
+
     for (let i = 0; i < 14; i++) {
       lastDay.setDate(lastDay.getDate() + 1);
       const day = `${lastDay.getFullYear()}-${
